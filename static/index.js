6b68ad83977549a318cd8d2361aee824fd21061a
Noah Snelson <noah.snelson@protonmail.com>
2018-12-20 23:20:13 -0900

    React rendering, but getting CORS error on fetching /translate endpoint.

--- static/index.js
+++ static/index.js
@@ -1,19 +1,43 @@
-const submitToAPI= async () =>{
-    let form = document.getElementById("input").elements['inputText'];
-    let result = document.getElementById("output");
-    let formText = form.value;
-    fetch("./translate?q=" + formText)
-        .then( (res)=>{
-            res.json().then( (data) =>{
-                result.innerHTML = data;
-                form.value = "";
-            });
-        })   
-        .catch((err) => {
-            console.log(err);
-            return;
+class TranslateInput extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            text : "Enter text here!",
+            output : ""
+        };
+    }
+    render(){
+        return(
+            <div>
+                <form id='input' onSubmit={this.submitToAPI}>
+                    <textarea id='inputText' cols="50" rows="7" wrap='hard' value={this.state.text} onChange={this.handleChange}></textarea>
+                    <input type='submit' value='Translate'/>
+                </form>
+                <p> Inupiaq: </p>
+                <p> {this.state.output} </p>
+            </div>
+        );
+    }
+    handleChange = (event) => {
+        this.setState({
+            text : event.target.value
         });
+    }
+    submitToAPI = async () => {
+        fetch('localhost:5000/translate?q=' + this.state.text)
+            .then((res) => {
+                res.json().then((data) => {
+                    alert(date);
+                    this.setState({
+                        output : data
+                    });
+                });
+            })   
+            .catch((err) => {
+                console.log(err);
+                return;
+            });
+    }
 }
-// TODO: refactor to React
 let root = document.getElementById("root");
-ReactDOM.render(<>, root);
+ReactDOM.render(<TranslateInput />, root);
