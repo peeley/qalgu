d410706f4857e4ebdae90424da13b574121be388
Noah Snelson <noah.snelson@protonmail.com>
2018-12-21 01:22:22 -0900

    Fixed Network error with event.preventDefault() . Translation now responsive.

--- static/index.js
+++ static/index.js
@@ -1,20 +1,24 @@
+const URL = 'http://192.168.1.139:5000/translate?q=';
+let root = document.getElementById("root");
+
 class TranslateInput extends React.Component{
     constructor(props){
         super(props);
         this.state = {
             text : "Enter text here!",
-            output : ""
+            output : "Translated text will appear here!"
         };
     }
     render(){
         return(
             <div>
                 <form id='input' onSubmit={this.submitToAPI}>
-                    <textarea id='inputText' cols="50" rows="7" wrap='hard' value={this.state.text} onChange={this.handleChange}></textarea>
+                    <textarea id='inputText' cols="50" rows="7" wrap='hard' 
+						value={this.state.text} onChange={this.handleChange}></textarea>
                     <input type='submit' value='Translate'/>
                 </form>
                 <p> Inupiaq: </p>
-                <p> {this.state.output} </p>
+                <textarea value={this.state.output} onChange={() => null}></textarea>
             </div>
         );
     }
@@ -22,22 +26,27 @@ class TranslateInput extends React.Component{
         this.setState({
             text : event.target.value
         });
+		if(event.target.value != ""){
+			this.submitToAPI(event);
+		}
+		else{
+			this.setState({ output : "" });
+		}
+		event.preventDefault();
     }
-    submitToAPI = async () => {
-        fetch('localhost:5000/translate?q=' + this.state.text)
-            .then((res) => {
-                res.json().then((data) => {
-                    alert(date);
-                    this.setState({
-                        output : data
-                    });
-                });
-            })   
-            .catch((err) => {
-                console.log(err);
-                return;
-            });
+    submitToAPI = async (event) => {
+		this.setState({ output : "" });
+		let url = URL + event.target.value;
+        fetch(url)
+		  .then(response => response.json())
+		  .then(json => {
+			this.setState({output : json });
+		  })
+		  .catch(err => {
+			  alert("Error: " + err);
+			  console.log(err)
+		  });
+		event.preventDefault();
     }
 }
-let root = document.getElementById("root");
 ReactDOM.render(<TranslateInput />, root);
